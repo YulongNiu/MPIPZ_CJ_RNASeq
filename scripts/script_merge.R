dce2276dc6f2717798b408246142281b7a8fc0ca
Yulong Niu <yulong.niu@hotmail.com>
2019-05-31 11:41:53 +0200

    first commit

---
+++ scripts/script_merge.R
@@ -0,0 +1,51 @@
+require('magrittr')
+require('doParallel')
+require('foreach')
+
+rawfqPath <- '/biodata/dep_psl/grp_rgo/yniu/CJ_201901_RNASeq'
+resFolder <- '/netscratch/dep_psl/grp_rgo/yniu/CJFe/raw_data'
+zcatPath <- '/bin/cat'
+ncore <- 40
+
+##~~~~~~~~~~~~~~~~~~~~~~~sample IDs~~~~~~~~~~~~~~~~~~~~~~~~~~
+rawfq <- dir(rawfqPath,
+             pattern = 'fastq.gz')
+
+fqs <- rawfq %>%
+  strsplit('_', fixed = TRUE) %>%
+  lapply('[', c(1, 2, 7)) %>%
+  sapply(paste, collapse = '_')
+
+## group fq gz files
+fqIdx <- split(seq_along(fqs), fqs)
+fqPrefix <- names(fqIdx)
+
+registerDoParallel(cores = ncore)
+
+foreach (i = seq_along(fqIdx), .combine = c) %dopar% {
+
+  ## input files
+  fqin <- fqIdx[[i]] %>%
+    {file.path(rawfqPath, rawfq[.])} %>%
+    paste(collapse = ' ')
+
+  fqout <- fqPrefix[i] %>%
+    paste0('.fq.gz') %>%
+    file.path(resFolder, .)
+
+  mergeC <- paste(zcatPath,
+                  fqin,
+                  '>',
+                  fqout)
+
+  print(mergeC)
+
+  system(mergeC)
+
+  return(NULL)
+}
+
+stopImplicitCluster()
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
